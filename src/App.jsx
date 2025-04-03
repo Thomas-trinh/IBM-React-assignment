984d6596df2d888c20272121ae30e753e3f8e039
Thomas-trinh <tungtrinh1611@gmail.com>
2025-04-03 15:23:07 +1100

    cart and checkout updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import ProductList from './Component/ProductPages';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import MyNavbar from './Component/Navbar';
+import LandingPage from './Component/LandingPage';
+import Cart from './Component/CartPage';
+import CheckoutPage from './Component/CheckoutPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<LandingPage />} />
         <Route path="/Product" element={<ProductList />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
   );
